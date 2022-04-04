22d5f30fa5419e05d3743761e329acbce4f95d6b
kausarahamed <galibhasan517@gmail.com>
2022-04-04 14:33:07 +0600

    RouteLink

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./component/About/About";
+import Blogs from "./component/Blogs/Blogs";
 import Dashboard from "./component/Dashboard/Dashboard";
 import Header from "./component/Header/Header";
 import Home from "./component/Home/Home";
+import NotFound from "./component/NotFound/NotFound";
+import Reviews from "./component/Reviews/Reviews";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
