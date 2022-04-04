9aa1bb559055d833cbb40d68b86f72e5aa1366e9
kausarahamed <galibhasan517@gmail.com>
2022-04-04 16:10:30 +0600

    dashboard compleate

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Dashboard = () => {
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((json) => setdata(json));
+  }, []);
   return (
     <div>
-      <h1>this is Dashboard </h1>
+      <div className="mt-10 grid grid-cols-2">
+        <div>
+          <AreaChart
+            width={630}
+            height={250}
+            data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+
+            <Area
+              type="monotone"
+              dataKey="sell"
+              stroke="#82ca9d"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+          </AreaChart>
+        </div>
+        <div>
+          <BarChart width={630} height={250} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="investment" fill="#8884d8" />
+            <Bar dataKey="revenue" fill="#82ca9d" />
+          </BarChart>
+        </div>
+      </div>
     </div>
   );
 };
